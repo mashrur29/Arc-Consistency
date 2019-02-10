05999a2267eb309f9e37872d7f0e8ce3286f27b2
mashrur29 <mashrur639@gmail.com>
2019-02-10 14:20:40 +0600

    AC 2 Init

---
+++ AC_2.py
@@ -0,0 +1,69 @@
+import queue
+import constraints
+
+def revise(xi, xj, constraint, domain):
+    revised = False
+    temp = []
+    for i in domain[xi]:
+        temp.append(i)
+
+    for x in temp:
+        single = False
+        for y in domain[xj]:
+            if(constraints.satisfy(x, y, constraint) == True):
+                single = True
+
+        if(single == False):
+            domain[xi].remove(x)
+            revised = True
+        if(len(domain[xi]) == 0):
+            break
+
+    return revised
+
+def solutionExists(domain):
+    for i in range(len(domain)):
+        if(len(domain[i]) == 0):
+            return False
+
+    return True
+
+def AC2(g, constraints, domain, node, edge, domainSize):
+    arcs = []
+
+    for i in g.edges:
+        arcs.append(i)
+    for i in g.edges:
+        arcs.append((i[1], i[0]))
+
+    for nod in range(node):
+        q = queue.Queue()
+        q1 = queue.Queue()
+
+        for i in arcs:
+            if(i[0]==nod and i[1]<nod):
+                q.put([i, constraints[i]])
+
+        for i in arcs:
+            if(i[1]==nod and i[0]<nod):
+                q1.put([i, constraints[i]])
+
+
+        while q.empty() is False:
+            top = q.get()
+            xi = top[0][0]
+            xj = top[0][1]
+            #print(top)
+            constraint = top[1]
+            if(revise(xi, xj, constraint, domain) == True):
+                if(len(domain[xi]) == 0):
+                    return False
+                for k in g.edges:
+                    if(k[0] == xi and k[1] != xj):
+                        q.put([(k[1], k[0]), constraints[k]])
+                    elif(k[1]==xi and k[0] != xj):
+                        q.put([(k[0], k[1]), constraints[k]])
+
+
+
+    return solutionExists(domain)
